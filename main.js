55041d1517abddd2a7db886796f794e559daaf7f
Noah Cress <56100756+N-Cress@users.noreply.github.com>
2024-09-05 18:50:27 +0000

    baseline

--- main.js
+++ main.js
@@ -77,6 +77,7 @@ function modifyModelMaterials(model) {
 
 // Handle window resize
 window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
